a1d7d059a94750c25c88b14bbb7e401fbaee4b85
Edinson <edinson.uribe01@gmail.com>
2026-06-14 18:15:37 +0200

    health bar added

--- Player/Items/projectile.gd
+++ Player/Items/projectile.gd
@@ -1,5 +1,6 @@
 class_name Projectile extends CharacterBody2D
 @export var SPEED = 300.0
+@export var damage_amount: float = 5.0
 func _physics_process(delta: float) -> void:
 	position += transform.x * SPEED *delta
 	move_and_slide()
@@ -7,7 +8,11 @@ func _physics_process(delta: float) -> void:
 	for i  in get_slide_collision_count():
 		var collision := get_slide_collision(i)
 		var collider := collision.get_collider()
+		if collider.has_method("take_damage"):
+			collider.take_damage(damage_amount)
+
 		queue_free()
+		
 		break
 	
 
