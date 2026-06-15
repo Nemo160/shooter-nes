1b9144ada6ebf706c715e684c0672deb87ffa365
Edinson <edinson.uribe01@gmail.com>
2026-06-15 14:51:56 +0200

    Added debug overlay

---
+++ DebugLayer.gd
@@ -0,0 +1,21 @@
+extends CanvasLayer
+
+var label: Label
+
+func _ready() -> void:
+	layer = 10
+	label = Label.new()
+	label.position = Vector2(5,5)
+	label.add_theme_color_override("font_color", Color.GREEN)
+	add_child(label)
+	
+
+# In DebugOverlay.gd
+func update_player(body: CharacterBody2D, dash_time_stopped: bool, air_jumped: bool) -> void:
+	label.text = "FPS: %d\nVelocity X: %.1f\nVelocity Y: %.1f\nDash ready: %s\nAir jumped: %s" % [
+		Engine.get_frames_per_second(),
+		body.velocity.x,
+		body.velocity.y,
+		"YES" if dash_time_stopped else "NO",
+		str(air_jumped)
+	]
